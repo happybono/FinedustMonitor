0f668a0b3ce72a1ad0b89f1f80e4df6fb47ed87a
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2019-12-27 16:58:31 +0900

    Update server.ino

--- FinedustMonitor/server.ino
+++ FinedustMonitor/server.ino
@@ -6,33 +6,99 @@
 // Released to the public domain
 //
 
-const char* host = "data.plaive.10make.com";
-String url = "/new_chart_line?api_key=";  
+const char* host_plaive = "data.plaive.10make.com";  
+const char* host_thingspeak = "api.thingspeak.com";
+const char* host_default = "finedustapi.10make.com";
+
 const int httpPort = 80;
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+WiFiClient client;
+String data;
+String contentType;
 
-void do_server(String APIKEY, int pm25, int pm10, float temperature) { 
-  WiFiClient client;
+void do_server_plaive(String api_key, int pm25, int pm10, float temperature) {
 
-  if (!client.connect(host, httpPort)) {
-    Serial.print("connection failed: ");
+  data = "api_key=" + String(api_key) + "&field1=" + String(pm25) + "&field2=" + String(pm10) + "&field3=" + String(temperature);
+  //contentType= "application/x-www-form-urlencoded";
+
+  //서버 통신 공식 client.println 을 사용하여야 합니다.
+  if(client.connect(host_plaive, httpPort)){
+    Serial.println("connected");
+    client.print("GET /insert.php?");
+    client.print(data); 
+    client.println(" HTTP/1.1");
+    client.println("Host: " + String(host_plaive)); // SERVER ADDRESS HERE AS WELL
+    client.println("Cache-Control: no-cache");
+    //client.println("Content-Type: application/xE-www-form-urlencoded"); 
+    //client.print("Content-Length: "); 
+    //client.println(data.length()); 
+    client.println("Connection: close");
+    client.println(); 
+    //client.print(data); 
+  }
+    
+  //서버 통신이 되지 않으면
+  else{
+    Serial.println("connection failed: ");
     return;
   }
+}
 
-  String payload = "pm25="+ String(pm25) +"&pm10="+ String(pm10)+"&temperature="+ String(temperature);
-  String getheader = "GET "+ String(url) + String(APIKEY)+ "&"+ payload +" HTTP/1.1";
-  client.println(getheader);
-  client.println("User-Agent: ESP8266 HAPPYBONO-DMK");  
-  client.println("Host: " + String(host));  
-  client.println("Connection: close");  
-  client.println();
+void do_server_thingspeak(String api_key, int pm25, int pm10, float temperature, String status) {
 
-  Serial.println(getheader);
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.println(line);
+  data = "api_key=" + String(api_key) + "&field1=" + String(pm25) + "&field2=" + String(pm10) + "&field3=" + String(temperature) + "&status=" + String(status);
+  //contentType= "application/x-www-form-urlencoded";
+
+  //서버 통신 공식 client.println 을 사용하여야 합니다.
+  if(client.connect(host_thingspeak, httpPort)){
+    Serial.println("connected");
+    client.print("GET /update?");
+    client.print(data); 
+    client.println(" HTTP/1.1");
+    client.println("Host: " + String(host_thingspeak)); // SERVER ADDRESS HERE AS WELL
+    client.println("Cache-Control: no-cache");
+    
+    //client.println("Content-Type: application/xE-www-form-urlencoded"); 
+    //client.print("Content-Length: "); 
+    //client.println(data.length()); 
+    client.println("Connection: close");
+    client.println();
+     
+    //client.print(data); 
   }
-  Serial.println("Send to Server.");
+    
+  //서버 통신이 되지 않으면
+  else{
+    Serial.println("connection failed: ");
+    return;
+  }
+}
+
+void do_server_default(String api_key, int pm25, int pm10, float temperature) {
+
+  data = "api_key=" + String(api_key) + "&pm25=" + String(pm25) + "&pm10=" + String(pm10) + "&temp=" + String(temperature);
+  contentType= "application/x-www-form-urlencoded";
+
+  //서버 통신 공식 client.println 을 사용하여야 합니다.
+  if(client.connect(host_default, httpPort)){
+    Serial.println("connected");
+    client.print("GET /insert.php?");
+    client.print(data); 
+    client.println(" HTTP/1.1");
+    client.println("Host: " + String(host_default)); // SERVER ADDRESS HERE AS WELL AS ABOVE.
+    client.println("Content-Type: application/xE-www-form-urlencoded"); 
+    client.print("Content-Length: "); 
+    client.println(data.length()); 
+    client.println("Connection: close");
+    client.println(); 
+    //client.print(data); 
+  }
+    
+  //서버 통신이 되지 않으면
+  else{
+    Serial.println("Connection Failed: ");
+    return;
+  }
 }
